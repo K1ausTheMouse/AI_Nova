464ea485396aa18bfe8f2c7888bd37e3e0755044
Klaus <klaudia@ampik.net>
2026-06-01 14:09:19 +0100

    Update del.py

--- AI-memory-system/del.py
+++ AI-memory-system/del.py
@@ -1,3 +1,5 @@
+# THIS IS ONLY TO DELETE ANYTHING IN THE MEMORY YOU DONT WANT NOVA TO STORE  
+
 from datetime import datetime
 import sqlite3
 
@@ -5,6 +7,8 @@ DB_PATH = "AI-memory-system/Memories.db"
 
 conn = sqlite3.connect(DB_PATH)
 cursor = conn.cursor()
-#don't accidentally dell somthing lol
+
+# UNCOMMENT THIS LINE TO DELETE, FUTURE: MAYBE AN APP TO CONTROL NOVA'S MEMORY 
+
 #cursor.execute("DELETE FROM memories WHERE id = ?", (,))
 conn.commit()
